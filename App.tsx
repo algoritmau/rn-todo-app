aca7934aec3996d5346f67bcb28005bb003615c0
Mauricio Paternina <spaceinvadev@icloud.com>
2022-06-02 22:40:53 -0500

    set up app theme

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import AppContainer from './src/components/app-container'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer>
+      <View style={styles.container}>
+        <Text>Hello, world!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </AppContainer>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fefefe',
     alignItems: 'center',
     justifyContent: 'center'
   }
